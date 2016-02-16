bb8d07a1c37084637a42c7d171c06800f31c74ed
vedraiyani <vedraiyani@gmail.com>
2016-02-16 13:21:21 +0530

    no item consideration is made dynamic in testcase generation

--- generate_testSet.R
+++ generate_testSet.R
@@ -6,9 +6,11 @@
 #generate_testSet<-function(n){
   
   #no of test rating
-  n=5;
+  n=20;
+  #no of test item
+  m=2;
   
-  testSet=matrix(0,n,3);
+  testSet=matrix(0,n*m,3);
   
   #generate random users
   test_users=sample(users,n);
@@ -18,14 +20,27 @@
   
   count=0
   for(u in 1:length(test_users)){
+    item_count=0;
     for(i in 1:length(test_items)){
-      if(rating_matrix[u,i]!=0){
-        count=count+1;
-        testSet[count,1]=u;
-        testSet[count,2]=i;
-        testSet[count,3]=rating_matrix[u,i];
-        rating_matrix[u,i]=0;
+      
+      if(item_count==m){
         break();
+      }
+      
+      ui=test_users[u];#user index
+      ii=test_items[i];#item index
+      
+      if(rating_matrix[ui,ii]!=0){
+      
+          count=count+1;
+          
+          testSet[count,1]=ui;
+          testSet[count,2]=ii;
+          testSet[count,3]=rating_matrix[ui,ii];
+          
+          rating_matrix[ui,ii]=0;
+          
+          item_count=item_count+1;
       }
     } 
   }
